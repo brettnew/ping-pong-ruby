9784d7ed819e5bb5ac860711a69d60986acac46e
Brett New and Julia Dickey <julia@>
2016-06-27 10:51:06 -0700

    Fixed order of specs

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -6,15 +6,15 @@ class Fixnum
     numbers_array
     pingpong_array = []
     numbers_array.each() do |number|
-        if number.%(15) == 0
-          pingpong_array.push("pingpong")
-        elsif number.%(5) == 0
-          pingpong_array.push("pong")
-        elsif number.%(3) == 0
-          pingpong_array.push("ping")
-        else
-          pingpong_array.push(number)
-        end
+      if number.%(15) == 0
+        pingpong_array.push("pingpong")
+      elsif number.%(5) == 0
+        pingpong_array.push("pong")
+      elsif number.%(3) == 0
+        pingpong_array.push("ping")
+      else
+        pingpong_array.push(number)
+      end
     end
     pingpong_array
   end
